99ab511de9593ed8cdffe4723d56eaa92aec2841
beatorizu <betrizaf@gmail.com>
2024-03-02 19:00:32 -0300

    Retrieve tournament by id

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from json import dump
-from os import makedirs
+from json import dump, load
+from os import makedirs, path
 from uuid import uuid4
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from models import Tournament
 
@@ -18,3 +18,12 @@ async def create_tournament(tournament: Tournament):
     id = uuid4()
     with open(f".tournaments/{id}.json", "w") as stream:
         dump({"id": f"{id}", **tournament.model_dump()}, stream)
+
+@app.get("/tournament/{id}")
+async def get_tournament(id: str):
+    if not path.exists(f".tournaments/{id}.json"):
+        raise HTTPException(status_code=404, detail=f"Tournament {id} not found")
+    with open(f".tournaments/{id}.json") as stream:
+        tournament = load(stream)
+    
+    return tournament
--- test_tournament.py
+++ test_tournament.py
@@ -16,3 +16,21 @@ def test_can_create_tournament():
 
     assert response.status_code == 200
     assert response.content["id"] is not None
+
+
+def test_can_retrieve_tournament_by_id():
+    expected_response = {
+        "id": "8581c72f-bc5d-4087-aaf8-2ef70c8bd542",
+        "name": "Wood Gladiators"
+    }
+
+    response = client.get(f"/tournament/{expected_response['id']}")
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+def test_cannot_retrieve_tournament_by_id():
+    response = client.get("/tournament/aff785a5-9341-4b1f-9cbe-b67c18ba728c")
+
+    assert response.status_code == 404
